aa9e541d3a64f165fe5d57de6b42171789613385
Sergio <sergio.rod.perez@outlook.es>
2020-06-02 09:44:02 -0500

    Updates

---
+++ precache-manifest.23a1a46b82939e72f60fbc2403ff64ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5e8ea4fa8248e15c0fdf85964ca60e",
+    "url": "/Proyect-Herramientas/index.html"
+  },
+  {
+    "revision": "3561b5a7f16bb452dbcf",
+    "url": "/Proyect-Herramientas/static/css/main.791a17dd.chunk.css"
+  },
+  {
+    "revision": "f5f7c9d7ea7f99a1048f",
+    "url": "/Proyect-Herramientas/static/js/2.ce137f91.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/Proyect-Herramientas/static/js/2.ce137f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337c541ec9ba1a885dac",
+    "url": "/Proyect-Herramientas/static/js/3.c1d4226c.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/Proyect-Herramientas/static/js/3.c1d4226c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f54659baf66fb47daf",
+    "url": "/Proyect-Herramientas/static/js/4.b16c1462.chunk.js"
+  },
+  {
+    "revision": "3561b5a7f16bb452dbcf",
+    "url": "/Proyect-Herramientas/static/js/main.857fe52e.chunk.js"
+  },
+  {
+    "revision": "f7ee1ebb0ad290bf6a1f",
+    "url": "/Proyect-Herramientas/static/js/runtime-main.3036942e.js"
+  }
+]);
